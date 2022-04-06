af723c798f2ebfc0219a05e6a8a7be3a2b2997f7
Rafa <88807518+RafaSantosBh@users.noreply.github.com>
2022-04-05 21:35:59 -0300

    Projeto Api

---
+++ apiCotemig/src/main/java/com/example/apiCotemig/ApiCotemigApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiCotemig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCotemigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCotemigApplication.class, args);
+	}
+
+}
---
+++ apiCotemig/src/test/java/com/example/apiCotemig/ApiCotemigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiCotemig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCotemigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
