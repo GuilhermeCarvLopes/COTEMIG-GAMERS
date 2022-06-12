d92fc4ff2648150e1b11fdc822bb6887ff2d7ce5
Guilherme Carvalho Lopes <101372301+GuilhermeCarvLopes@users.noreply.github.com>
2022-06-12 18:21:00 -0300

    new arqs

---
+++ apiCotemigGamers/src/test/java/com/example/apiCotemigGamers/ApiCotemigGamersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiCotemigGamers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCotemigGamersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
